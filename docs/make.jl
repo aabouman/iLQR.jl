55c175780d0f7671283568708225a90732968ab1
Alexander A. Bouman <alex.bouman@gmail.com>
2021-01-13 11:48:59 -0500

    Changed filename

--- docs/make.jl
+++ docs/make.jl
@@ -3,13 +3,16 @@ using iLQR
 
 
 Documenter.makedocs(
-    sitename = "iLQR.jl",
-    repo = "https://github.com/aabouman/iLQR.jl",
+    # sitename = "iLQR.jl",
+    # repo = "https://github.com/aabouman/iLQR.jl",
     pages = [
         "Home" => "index.md",
         # "Documentation" => "documentation.md",
         # "Tutorial" => "tutorial.md"
     ]
+)
+deploydocs(
+    repo = "https://github.com/aabouman/iLQR.jl",
 )
 
 # deploydocs(
