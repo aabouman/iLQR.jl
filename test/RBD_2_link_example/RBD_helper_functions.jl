3b1f1b109f0bda4ad5551b3cf147e0ef951389d7
Alexander A. Bouman <alex.bouman@gmail.com>
2021-01-13 16:16:04 -0500

    Wrapped RBD.jl

--- test/RBD_2_link_example/RBD_helper_functions.jl
+++ test/RBD_2_link_example/RBD_helper_functions.jl
@@ -2,9 +2,52 @@ using RigidBodyDynamics, Attitude
 
 
 # Load up model URDF
-urdf = include(joinpath(dirname(@__FILE__),"../urdf/2Dof_arm.urdf"))
-mechanism = parse_urdf(urdf)
-const statecache = StateCache(mechanism)
+urdf = joinpath(dirname(@__FILE__) ,"../urdf/2Dof_arm.urdf")
+mechanism = parse_urdf(urdf, gravity = [0.; 0.; 0.], floating = true)
+state = MechanismState(mechanism)
+set_configuration!(state, [0.,0.,0.,1,.5,.75,1.,0.,0.])
+
+
+"""
+Converts quaternion orienation state to an MRP orientaiton state
+"""
+function RBD_to_iLQR_state(x::AbstractVector{T}) where T
+    q = x[1:4]
+    return [p_from_q(q); x[5:end]]
+end
+
+
+"""
+Converts MRP orienation state to an quaternion orientaiton state
+"""
+function iLQR_to_RBD_state(x::AbstractVector{T}) where T
+    p = x[1:3]
+    return [q_from_p(p); x[4:end]]
+end
+
+
+# Continous function wrapped around RBD library's forward dynamics function
+function continuous_dynamics(x̅ᵢ::AbstractVector{T}, u̅ᵢ::AbstractVector) where T
+    p = x̅ᵢ[1:3];  r = x̅ᵢ[4:6];   θ = x̅ᵢ[7:8]    # pose
+    ω = x̅ᵢ[9:11]; v = x̅ᵢ[12:14]; θ̇ = x̅ᵢ[15:16]  # velocity
+
+    # now we convert it to a state for RBD
+    𝑞 = [q_from_p(p); r; θ]; 𝑣 = [ω; v; θ̇];
+
+    state = MechanismState{T}(mechanism)
+    set_configuration!(state, 𝑞); set_velocity!(state, 𝑣);
+
+    # get the dynamics for v (this state is the same for both)
+    M = Array(mass_matrix(state))
+    if hasnan(M)
+        return NaN * x̅ᵢ
+    else
+        v̇ = M \ (-dynamics_bias(state) + u̅ᵢ) # dynamics
+        q̇ = [pdot_from_w(p,ω); v; θ̇]        # kinematics
+
+        return [q̇;v̇]
+    end
+end
 
 
 """
@@ -12,32 +55,13 @@ Wraps RBD.jl dynamics function with a rk4 integrator descrete dynamics function
 for use with the iLQR.jl package.
 """
 function dynamicsf(x::AbstractVector, u::AbstractVector)
-    # Continous function wrapped around RBD library's forward dynamics function
-    function continuous_dynamics(x̅ᵢ::AbstractVector{T}, u̅ᵢ::AbstractVector{T}) where T
-        p = x̅ᵢ[1:3];   r = x̅ᵢ[4:6];   θ = x̅ᵢ[7:9]    # pose
-        ω = x̅ᵢ[10:12]; v = x̅ᵢ[13:15]; θ̇ = x̅ᵢ[16:18]  # velocity
-        # now we convert it to a state for RBD
-        state = statecache[T]
-        copyto!(state, [q_from_p(p); x̅ᵢ[4:end]])
-        # get the dynamics for v (this state is the same for both)
-        M = Array(mass_matrix(state))
-        if hasnan(M)
-            return NaN * x̅ᵢ
-        else
-            v̇ = M \ (-dynamics_bias(state) + u̅ᵢ) # dynamics
-            q̇ = [pdot_from_w(p,ω); v; θ̇]        # kinematics
-
-            return [q̇;v̇]
-        end
-    end
-
     # RK 4 integration
     k1 = Δt * continuous_dynamics(x, u)
     k2 = Δt * continuous_dynamics(x + k1/2, u)
     k3 = Δt * continuous_dynamics(x + k2/2, u)
     k4 = Δt * continuous_dynamics(x + k3, u)
 
-    new_state = x_n + (1/6) * (k1 + 2 * k2 + 2 * k3 + k4)
+    new_state = x + (1/6) * (k1 + 2 * k2 + 2 * k3 + k4)
     return new_state
 end
 
@@ -63,7 +87,7 @@ end
 Immediate cost function, evaluated after each time step in the trajectory
 """
 function final_cost(x̅ₙ::AbstractVector)
-    pos = x̅ᵢ[1:8]
+    pos = x̅ₙ[1:8]
 
     euclidean_penalty = sum((target_pose .- pos).^2)
 
--- test/RBD_2_link_example/animate_RBD_2_link.jl
+++ test/RBD_2_link_example/animate_RBD_2_link.jl
@@ -1,40 +1,45 @@
 using iLQR
+using MeshCatMechanisms
 
-target_pose = [0, 0, 0, .5, .25, .6, 1., .3]
+# set simulation parameters- length of time step, # of steps
+Δt = 0.01; num_steps = 1000;
+# target pose is size 8 (3 orr, 3 pos, 2 joint)
+target_pose = [0., 0., 0., .5, .25, .6, 1., .3]
 
+# add in helper functions
 include("RBD_helper_functions.jl")
 
-maximum_iterations = convert(Int64, 1e6)
-tolerance = 1e-6
-num_steps = 1000
+# set solver parameters, # of iLQR iterations and tolerance til convergence
+maximum_iterations = convert(Int64, 1e6); tolerance = 1e-6;
 
-input_traj = zeros(num_steps, n_links)
-state_traj = zeros(num_steps+1, n_links*2)
-state_traj[1,:] = [.1, -.1, 0., 0.] #rand(4)
+# initalize trajectories
+input_traj = zeros(num_steps, length(target_pose))
+state_traj = zeros(num_steps+1, length(target_pose)*2)
+# Set inital configuration
+state_traj[1,:] = RBD_to_iLQR_state([configuration(state);velocity(state)])
 for i = 1:num_steps
       state_traj[i+1,:] .= dynamicsf(state_traj[i,:], input_traj[i, :])
 end
 
 
 # Build vector of time steps
-t = [0.:convert(Float64, num_steps);]
+ts = [0.:Δt:(Δt*num_steps);]
 # Fit using iLQR
 (x̅ᶠ, u̅ᶠ) = iLQR.fit(state_traj, input_traj, dynamicsf, immediate_cost,
-                    final_cost; max_iter = maximum_iterations, tol = tolerance,
-                    )
+                    final_cost; max_iter = maximum_iterations, tol = tolerance)
+
+x_RBD = zeros(size(x̅ᶠ)[1], size(x̅ᶠ)[2]+1)
+for i in 1:size(x̅ᶠ)[1]
+    x_RBD[i,:] .= iLQR_to_RBD_state(x̅ᶠ[i,:])
+end
+
+q_RBD = x_RBD[:,1:9]
+v_RBD = x_RBD[:,10:17]
+
+q_RBD_lol = [q_RBD[i,:] for i in 1:size(q_RBD)[1]]
+v_RBD_lol = [x_RBD[i,:] for i in 1:size(x_RBD)[1]]
 
 println("Animating...")
-df = 10
-anim = @animate for t = 1:df:length(t)
-    p4 = plot([0, l₁ * cos(x̅ᶠ[t, 1]), l₁ * cos(x̅ᶠ[t, 1]) + l₂ * cos(x̅ᶠ[t, 1] + x̅ᶠ[t, 2])],
-              [0, l₁ * sin(x̅ᶠ[t, 1]), l₁ * sin(x̅ᶠ[t, 1]) + l₂ * sin(x̅ᶠ[t, 1] + x̅ᶠ[t, 2])],
-              alpha=0.5, linewidth=5, color=:red, markershape=:circle, label="",
-              aspect_ratio=:equal, size=(400,400));
-    plot!(p4, [0, l₁ * cos(x̅ᶠ[t, 1]), l₁ * cos(x̅ᶠ[t, 1]) + l₂ * cos(x̅ᶠ[t, 1] + x̅ᶠ[t, 2])],
-          [0, l₁ * sin(x̅ᶠ[t, 1]), l₁ * sin(x̅ᶠ[t, 1]) + l₂ * sin(x̅ᶠ[t, 1] + x̅ᶠ[t, 2])],
-          alpha=0.2, linewidth=5, color=:blue, markershape=:circle, label="",)
-    xlims!((-2, 2))
-    ylims!((-2, 2))
-end
-save_loc = joinpath(dirname(@__FILE__),"../figures/iLQR_2_link_quad_4.gif")
-gif(anim, save_loc, fps = 20)
+mvis = MechanismVisualizer(mechanism, URDFVisuals(urdf));
+open(mvis)
+MeshCatMechanisms.animate(mvis, ts, q_RBD_lol; realtimerate = 1.);
