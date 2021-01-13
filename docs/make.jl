2adb0ec0637a05b9004108e3cef202eb18e31a7f
Alexander A. Bouman <alex.bouman@gmail.com>
2021-01-13 13:00:57 -0500

    changed make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -1,31 +1,15 @@
-using Documenter
-using iLQR
+using Documenter, iLQR
 
-
-# Documenter.makedocs(
-#     sitename = "iLQR.jl",
-#     repo = "https://github.com/aabouman/iLQR.jl",
-#     pages = [
-#         "Home" => "index.md",
-#         # "Documentation" => "documentation.md",
-#         # "Tutorial" => "tutorial.md"
-#     ]
-# )
-makedocs(
+Documenter.makedocs(
     sitename = "iLQR.jl",
     repo = "https://github.com/aabouman/iLQR.jl",
     pages = [
-            "Home" => "index.md",
-        ]
-)
-deploydocs(
-    repo = "https://github.com/aabouman/iLQR.jl",
-    branch = "gh-pages",
+        "Home" => "index.md",
+        "Documentation" => "documentation.md",
+        "Tutorial" => "tutorial.md"
+    ]
 )
 
-# deploydocs(
-#     repo = "github.com/m3g/PDBTools.git",
-#     target = "build",
-#     branch = "gh-pages",
-#     # versions = ["stable" => "v^", "v#.#" ],
-# )
+deploydocs(;
+    repo="https://github.com/aabouman/iLQR.jl",
+)
